8349e0873ed9b6e79c0667f5367d8afe93e007b9
pranay-kumar-reddy <ppkreddy0212@gmail.com>
2020-04-23 00:38:18 +0530

    added

--- project.erl
+++ project.erl
@@ -1,16 +1,38 @@
 -module(project).
--export([main/0,create_process/1,for/2]).
-
-for(0,_) -> []; 
-
-for(N,Term) when N > 0 -> 
-   io:fwrite("Hello~n"), 
-   [Term|for(N-1,Term)].
-
-create_process(a) -> [].
-
-
-
+-export([main/0,create_process/0,for/3,for2/3]).
 main() ->
-    A0 = array:new(10)
-    spawn('project',create_process,[])
+  	Lst = [0.1,0.0,0.3,0.7],
+ 	Lst1 = lists:reverse(lists:sort(Lst)),
+	Rho = 	for2(Lst1,0,0),
+      	L = for(1, 4, fun() -> spawn( 'project',create_process,[] ) end ),
+      	Map=maps:from_list(lists:zip(L,Lst1)),
+      	lists:foreach(fun(A)-> A ! {map,Map,Rho} end,L),
+      	io:format("~w~n",[Lst1]),
+      	io:format("~w~n",[Map]).
+  
+   
+create_process() ->
+   	receive
+       	{map,Map,Rho}->
+           	Mp=Map,
+           	io:format("~w~n",[Mp]),
+io:format("~w~n",[Rho])
+   	end,
+   	Myval = rand:uniform(2) -1 .
+   
+	
+for2([Ls|Lst1],Sum,Cnt) ->
+        if  
+Sum+Ls =< 0.9  ->  
+			for2(Lst1,Sum+Ls,Cnt+1);
+        	true -> 
+			Cnt+1
+ 	  end.
+  
+for( N, N, F )  -> [F()];
+for( I, N, F ) ->
+     io:format("process created: ~w~n", [I] ),
+     [F() | for(I+1, N, F)].
+ 
+ 
+ 
